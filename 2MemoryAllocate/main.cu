f0e56c95ea43cd18d072a2a431709d6be3398884
pengxianjun <pengxianjun@misrobot.com>
2015-12-12 23:11:43 +0800

    2MemoryAllocate

---
+++ 2MemoryAllocate/main.cu
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+using namespace std;
+
+
+int main()
+{
+	float * pDeviceData = nullptr;
+	int width = 10 * sizeof(float);
+	int height = 10 * sizeof(float);
+	size_t pitch;
+
+	cudaError err = cudaSuccess;
+
+	//1 use cudaMallocPitch function
+	err = cudaMallocPitch(&pDeviceData, &pitch, width, height);		//注意这里的width和height的单位为字节数
+	if (err != cudaSuccess)
+	{
+		cout << "call cudaMallocPitch fail!!!" << endl;
+		exit(1);
+	}
+	cout << "width: " << width << endl;
+	cout << "height: " << height << endl;
+	cout << "pitch: " << pitch << endl;
+
+	//error code:不能在主机上操作设备内存
+
+	/*for (int r = 0; r < height; ++r)
+	{
+	for (int c = 0; c < width; ++c)
+	{
+	float data = *(pDeviceData + pitch * r + c);
+	cout << data << " ";
+	}
+	cout << endl;
+	}*/
+
+	//2 use cudaMalloc3D
+	cudaPitchedPtr pitchPtr;
+	cudaExtent extent;
+	extent.width = 10 * sizeof(float);
+	extent.height = 22 * sizeof(float);
+	extent.depth = 33 * sizeof(float);
+
+	err = cudaMalloc3D(&pitchPtr, extent);
+	if (err != cudaSuccess)
+	{
+		cout << "call cudaMalloc3D fail!!!" << endl;
+		exit(1);
+	}
+	cout << "\n\n";
+	cout << "width: " << extent.width << endl;			//输出申请内存的初始值
+	cout << "height: " << extent.height << endl;
+	cout << "depth: " << extent.depth << endl;
+
+	cout << endl;
+	cout << "pitch: " << pitchPtr.pitch << endl;		//输出实际的宽度值
+	cout << "xsize: " << pitchPtr.xsize << endl;		//有效宽度--等于extent.width
+	cout << "ysize: " << pitchPtr.ysize << endl;		//有效高度--等于extent.height
+
+	cudaFree(pDeviceData);
+	cudaFree(pitchPtr.ptr);
+	cin.get();
+	return 0;
+}
+
